25c922ebb999bee5dd472dc9a1b275559803d308
Peter Bartholet <peter.bartholet@kfnmail.ch>
2017-11-19 17:13:30 +0100

    more files removed from git

--- Backend/express-ts/app/controllers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-/* app/controllers/index.ts */
-__export(require('./userController'));
-//# sourceMappingURL=index.js.map
