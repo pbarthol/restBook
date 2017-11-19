57e62fce97957d7e5fb8c1a0fa3086755de6a8f8
Peter Bartholet <peter.bartholet@kfnmail.ch>
2017-11-19 17:10:27 +0100

    remove folder build from git

--- Backend/express-ts/build/config/database.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Created by Peter on 12.11.2017.
- */
-exports.database = 'mongodb://localhost:27017/restBook';
-exports.secret = 'iloverentany';
