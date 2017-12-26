3cb44252e31e3f1845cea189599a18fe0eb50ff1
Peter Bartholet <peter.bartholet@kfnmail.ch>
2017-12-26 19:05:47 +0100

    add save user

--- Backend/express-ts/app/models/user.ts
+++ Backend/express-ts/app/models/user.ts
@@ -2,11 +2,12 @@
  * Created by Peter on 12.11.2017.
  */
 "use strict";
-let mongoose = require('mongoose');
-let bcrypt = require('bcryptjs');
-let Schema = mongoose.Schema;
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
 // Note collection at mongo db must be 'users' -> Plural
-let user = new Schema({
+let userSchema = new Schema({
     username: String,
     password: String,
     salutation: String,
@@ -20,27 +21,28 @@ let user = new Schema({
     createdDate: Date,
     admin: Boolean
 });
-// // encrypt password before saving
-// user.pre('save', function (next) {
-//     let user = this;
-//     if (user.isModified('password') || user.isNew) {
-//         bcrypt.genSalt(10, function (err, salt) {
-//             if (err) {
-//                 return next(err);
-//             }
-//             bcrypt.hash(user.password, salt, function (err, hash) {
-//                 if (err) {
-//                     return next(err);
-//                 }
-//                 user.password = hash;
-//                 next();
-//             });
-//         });
-//     } else {
-//         return next();
-//     }
-// });
+// encrypt password before saving
+userSchema.pre('save', function (next) {
+    // console.log('this:', this)
+    var user = this;
+    if (user.isModified('password') || user.isNew) {
+        bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
 
 
 // Create a model based on the schema
-module.exports = mongoose.model('User', user);
+module.exports = mongoose.model('User', userSchema);
