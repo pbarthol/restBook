348dfac381161c833fe42808a362162ed13a9a56
Peter Bartholet <peter.bartholet@kfnmail.ch>
2018-01-23 21:01:01 +0100

    file upload with resize works

--- Backend/express-ts/app/controllers/uploadController.ts
+++ Backend/express-ts/app/controllers/uploadController.ts
@@ -5,6 +5,7 @@ import { Router, Request, Response } from 'express';
 const router: Router = Router();
 var multer = require('multer');
 var fs = require('fs');
+var gm = require('gm').subClass({ imageMagick: true });
 
 
 // router.post('/', function(req, res, next) {
@@ -24,29 +25,32 @@ router.post('/', upload.array('restaurantPicture',8), function (req, res) {
          stored in the variable "originalname". **/
         var tmp_path = file.path;
         var target_path = 'public/images/' + file.originalname;
-
         /** A better way to copy the uploaded file. **/
         var src = fs.createReadStream(tmp_path);
         var dest = fs.createWriteStream(target_path);
         src.pipe(dest);
+        // resize uploaded image
+        var thumbnail_width = 100; // 100px = thumbnail width
+        var resize_width = 500; // 500px = resized width
+        var thumbnail_path_name: string;
+        var resized_path_name: string;
+        var thumbnail_extension = target_path.substring(target_path.indexOf(".") + 1);
+        thumbnail_path_name = target_path.substring(0, target_path.indexOf(".")) + "_thumbnail." + thumbnail_extension;
+        resized_path_name = target_path.substring(0, target_path.indexOf(".")) + "_resized." + thumbnail_extension;
+        gm(target_path)
+            .resize(thumbnail_width)
+            .autoOrient()
+            .write(thumbnail_path_name, function (err) {
+                if (!err) console.log(' hooray! ');
+            });
+        gm(target_path)
+            .resize(resize_width)
+            .autoOrient()
+            .write(resized_path_name, function (err) {
+                if (!err) console.log(' hooray! ');
+            });
     });
-
     res.status(204).end()
 });
-
-
-// function addUploadRecord(req, res){
-//     let upload = new Upload();
-//     upload.originalname = req.file.originalname;
-//     upload.storagename = req.file.path;
-//     upload.save(function (err, upload) {
-//         if (err) {
-//             console.error(err);
-//             return res.send(err);
-//         }
-//         // return res.send(upload.toJSON());
-//     });
-// }
-
 
 export const UploadController: Router = router;
